e86f29b34625b2ad9e6b57840b1f140743bc2f07
Lapis <45414333+Lapis256@users.noreply.github.com>
2025-01-05 07:27:27 +0900

    bump version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
         const val name = "AE2 MEGA Things"
         const val description = "AE2 MEGA Things"
         const val license = "MIT"
-        const val version = "2.0.0"
+        const val version = "2.0.1"
         const val group = "io.github.lapis256"
         const val author = "Lapis256"
         const val repositoryUrl = "https://github.com/Lapis256/AE2MEGAThings"

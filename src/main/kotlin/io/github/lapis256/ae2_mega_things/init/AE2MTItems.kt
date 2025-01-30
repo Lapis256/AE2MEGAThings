3ef2f7a973514a341ebd03b5e68769240d01d51d
Lapis <45414333+Lapis256@users.noreply.github.com>
2025-01-30 20:03:23 +0900

    Possible fix for #3: Use a less likely failure method to prevent startup crash

--- src/main/kotlin/io/github/lapis256/ae2_mega_things/init/AE2MTItems.kt
+++ src/main/kotlin/io/github/lapis256/ae2_mega_things/init/AE2MTItems.kt
@@ -14,6 +14,7 @@ import io.github.lapis256.ae2_mega_things.util.componentItem
 import io.github.lapis256.ae2_mega_things.util.kilobytes
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.ItemLike
+import java.util.function.Supplier
 
 
 typealias MEGADISKDriveFactory = (component: ItemLike, housing: ItemLike, kilobyte: Int, idleDrain: Double) -> AbstractDISKDrive
@@ -25,33 +26,33 @@ object AE2MTItems {
 
     val MEGA_ITEM_DISK_HOUSING = housing("MEGA Item", "mega_item")
 
-    val ITEM_DISK_1M = megaItemDrive(MEGAItems.TIER_1M)
-    val ITEM_DISK_4M = megaItemDrive(MEGAItems.TIER_4M)
-    val ITEM_DISK_16M = megaItemDrive(MEGAItems.TIER_16M)
-    val ITEM_DISK_64M = megaItemDrive(MEGAItems.TIER_64M)
-    val ITEM_DISK_256M = megaItemDrive(MEGAItems.TIER_256M)
+    val ITEM_DISK_1M = megaItemDrive { MEGAItems.TIER_1M }
+    val ITEM_DISK_4M = megaItemDrive { MEGAItems.TIER_4M }
+    val ITEM_DISK_16M = megaItemDrive { MEGAItems.TIER_16M }
+    val ITEM_DISK_64M = megaItemDrive { MEGAItems.TIER_64M }
+    val ITEM_DISK_256M = megaItemDrive { MEGAItems.TIER_256M }
 
     val MEGA_ITEM_DISKS = setOf(ITEM_DISK_1M, ITEM_DISK_4M, ITEM_DISK_16M, ITEM_DISK_64M, ITEM_DISK_256M)
 
 
     val FLUID_DISK_HOUSING = housing("ME Fluid", "fluid")
 
-    val FLUID_DISK_1K = fluidDrive(StorageTier.SIZE_1K)
-    val FLUID_DISK_4K = fluidDrive(StorageTier.SIZE_4K)
-    val FLUID_DISK_16K = fluidDrive(StorageTier.SIZE_16K)
-    val FLUID_DISK_64K = fluidDrive(StorageTier.SIZE_64K)
-    val FLUID_DISK_256K = fluidDrive(StorageTier.SIZE_256K)
+    val FLUID_DISK_1K = fluidDrive { StorageTier.SIZE_1K }
+    val FLUID_DISK_4K = fluidDrive { StorageTier.SIZE_4K }
+    val FLUID_DISK_16K = fluidDrive { StorageTier.SIZE_16K }
+    val FLUID_DISK_64K = fluidDrive { StorageTier.SIZE_64K }
+    val FLUID_DISK_256K = fluidDrive { StorageTier.SIZE_256K }
 
     val FLUID_DISKS = setOf(FLUID_DISK_1K, FLUID_DISK_4K, FLUID_DISK_16K, FLUID_DISK_64K, FLUID_DISK_256K)
 
 
     val MEGA_FLUID_DISK_HOUSING = housing("MEGA Fluid", "mega_fluid")
 
-    val FLUID_DISK_1M = megaFluidDrive(MEGAItems.TIER_1M)
-    val FLUID_DISK_4M = megaFluidDrive(MEGAItems.TIER_4M)
-    val FLUID_DISK_16M = megaFluidDrive(MEGAItems.TIER_16M)
-    val FLUID_DISK_64M = megaFluidDrive(MEGAItems.TIER_64M)
-    val FLUID_DISK_256M = megaFluidDrive(MEGAItems.TIER_256M)
+    val FLUID_DISK_1M = megaFluidDrive { MEGAItems.TIER_1M }
+    val FLUID_DISK_4M = megaFluidDrive { MEGAItems.TIER_4M }
+    val FLUID_DISK_16M = megaFluidDrive { MEGAItems.TIER_16M }
+    val FLUID_DISK_64M = megaFluidDrive { MEGAItems.TIER_64M }
+    val FLUID_DISK_256M = megaFluidDrive { MEGAItems.TIER_256M }
 
     val MEGA_FLUID_DISKS = setOf(FLUID_DISK_1M, FLUID_DISK_4M, FLUID_DISK_16M, FLUID_DISK_64M, FLUID_DISK_256M)
 
@@ -76,12 +77,12 @@ object AE2MTItems {
 
     fun housing(prefix: String, idPrefix: String) = register("$prefix DISK Housing", "${idPrefix}_disk_housing") { DISKHousing() }
 
-    fun drive(typeName: String, idPrefix: String, tier: StorageTier, housing: ItemLike, factory: MEGADISKDriveFactory) =
-        register("${tier.namePrefix.replace("m", "M")} $typeName DISK Drive", "${idPrefix}_disk_drive_${tier.namePrefix}") {
+    fun drive(typeName: String, idPrefix: String, tierSupplier: Supplier<StorageTier>, housing: ItemLike, factory: MEGADISKDriveFactory) =
+        tierSupplier.get().let { tier -> register("${tier.namePrefix.replace("m", "M")} $typeName DISK Drive", "${idPrefix}_disk_drive_${tier.namePrefix}") {
             factory(tier.componentItem(), housing, tier.kilobytes(), tier.idleDrain).also { DISK_DRIVES.add(it) }
-        }
+        }}
 
-    private fun megaItemDrive(tier: StorageTier) = drive("MEGA Item", "item", tier, MEGA_ITEM_DISK_HOUSING, ::ItemDISKDrive)
-    private fun fluidDrive(tier: StorageTier) = drive("ME Fluid", "fluid", tier, FLUID_DISK_HOUSING, ::FluidDISKDrive)
-    private fun megaFluidDrive(tier: StorageTier) = drive("MEGA Fluid", "fluid", tier, MEGA_FLUID_DISK_HOUSING, ::FluidDISKDrive)
+    private fun megaItemDrive(tierSupplier: Supplier<StorageTier>) = drive("MEGA Item", "item", tierSupplier, MEGA_ITEM_DISK_HOUSING, ::ItemDISKDrive)
+    private fun fluidDrive(tierSupplier: Supplier<StorageTier>) = drive("ME Fluid", "fluid", tierSupplier, FLUID_DISK_HOUSING, ::FluidDISKDrive)
+    private fun megaFluidDrive(tierSupplier: Supplier<StorageTier>) = drive("MEGA Fluid", "fluid", tierSupplier, MEGA_FLUID_DISK_HOUSING, ::FluidDISKDrive)
 }
--- src/main/kotlin/io/github/lapis256/ae2_mega_things/integration/appmek/init/AMIntegrationItems.kt
+++ src/main/kotlin/io/github/lapis256/ae2_mega_things/integration/appmek/init/AMIntegrationItems.kt
@@ -5,27 +5,28 @@ import gripe._90.megacells.definition.MEGAItems
 import io.github.lapis256.ae2_mega_things.init.AE2MTItems.drive
 import io.github.lapis256.ae2_mega_things.init.AE2MTItems.housing
 import io.github.lapis256.ae2_mega_things.integration.appmek.item.ChemicalDISKDrive
+import java.util.function.Supplier
 
 
 object AMIntegrationItems {
     val CHEMICAL_DISK_HOUSING = housing("ME Chemical", "chemical")
 
-    val CHEMICAL_DISK_1K = chemicalDrive(StorageTier.SIZE_1K)
-    val CHEMICAL_DISK_4K = chemicalDrive(StorageTier.SIZE_4K)
-    val CHEMICAL_DISK_16K = chemicalDrive(StorageTier.SIZE_16K)
-    val CHEMICAL_DISK_64K = chemicalDrive(StorageTier.SIZE_64K)
-    val CHEMICAL_DISK_256K = chemicalDrive(StorageTier.SIZE_256K)
+    val CHEMICAL_DISK_1K = chemicalDrive { StorageTier.SIZE_1K }
+    val CHEMICAL_DISK_4K = chemicalDrive { StorageTier.SIZE_4K }
+    val CHEMICAL_DISK_16K = chemicalDrive { StorageTier.SIZE_16K }
+    val CHEMICAL_DISK_64K = chemicalDrive { StorageTier.SIZE_64K }
+    val CHEMICAL_DISK_256K = chemicalDrive { StorageTier.SIZE_256K }
 
     val CHEMICAL_DISKS = setOf(CHEMICAL_DISK_1K, CHEMICAL_DISK_4K, CHEMICAL_DISK_16K, CHEMICAL_DISK_64K, CHEMICAL_DISK_256K)
 
 
     val MEGA_CHEMICAL_DISK_HOUSING = housing("MEGA Chemical", "mega_chemical")
 
-    val CHEMICAL_DISK_1M = megaChemicalDrive(MEGAItems.TIER_1M)
-    val CHEMICAL_DISK_4M = megaChemicalDrive(MEGAItems.TIER_4M)
-    val CHEMICAL_DISK_16M = megaChemicalDrive(MEGAItems.TIER_16M)
-    val CHEMICAL_DISK_64M = megaChemicalDrive(MEGAItems.TIER_64M)
-    val CHEMICAL_DISK_256M = megaChemicalDrive(MEGAItems.TIER_256M)
+    val CHEMICAL_DISK_1M = megaChemicalDrive { MEGAItems.TIER_1M }
+    val CHEMICAL_DISK_4M = megaChemicalDrive { MEGAItems.TIER_4M }
+    val CHEMICAL_DISK_16M = megaChemicalDrive { MEGAItems.TIER_16M }
+    val CHEMICAL_DISK_64M = megaChemicalDrive { MEGAItems.TIER_64M }
+    val CHEMICAL_DISK_256M = megaChemicalDrive { MEGAItems.TIER_256M }
 
     val MEGA_CHEMICAL_DISKS = setOf(CHEMICAL_DISK_1M, CHEMICAL_DISK_4M, CHEMICAL_DISK_16M, CHEMICAL_DISK_64M, CHEMICAL_DISK_256M)
 
@@ -33,6 +34,6 @@ object AMIntegrationItems {
         // NO-OP
     }
 
-    private fun chemicalDrive(tier: StorageTier) = drive("ME Chemical", "chemical", tier, CHEMICAL_DISK_HOUSING, ::ChemicalDISKDrive)
-    private fun megaChemicalDrive(tier: StorageTier) = drive("MEGA Chemical", "chemical", tier, MEGA_CHEMICAL_DISK_HOUSING, ::ChemicalDISKDrive)
+    private fun chemicalDrive(tierSupplier: Supplier<StorageTier>) = drive("ME Chemical", "chemical", tierSupplier, CHEMICAL_DISK_HOUSING, ::ChemicalDISKDrive)
+    private fun megaChemicalDrive(tierSupplier: Supplier<StorageTier>) = drive("MEGA Chemical", "chemical", tierSupplier, MEGA_CHEMICAL_DISK_HOUSING, ::ChemicalDISKDrive)
 }
